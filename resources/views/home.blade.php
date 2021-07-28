0b9e70830c9d90d2a6fea77099e0ad6df7cbb0ce
TatuMon <ezurkanmn@gmail.com>
2021-07-28 17:32:32 -0300

    IGDB api config

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
         <link href="css/app.css" rel="stylesheet">
     </head>
     <body>
-        <form method="POST" action="https://id.twitch.tv/oauth2/token?client_id=qn51e547lyyo2n2c6chxiyhaccerzd&client_secret=31i49du7j4n71eyjkrtjyound50bmf&grant_type=client_credentials">
+        <form method="POST" action="games">
             <input type="submit" value="post"/>
         </form>
     </body>
