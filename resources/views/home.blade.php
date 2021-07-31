2ee6d1a3265a1deb4c441d84db967751cfd58dd6
TatuMon <ezurkanmn@gmail.com>
2021-07-31 19:42:19 -0300

    Create game card component and it's scss file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,12 +14,7 @@
     <body>
         <x-header />
         @foreach ($games as $game)
-            <div class="game-card">
-                <h1 class="game-name">{{ $game->name }}</h1>
-                <a href="/games/{{ $game->slug }}"><img src="{{ str_replace('t_thumb', 't_cover_big', $game->cover['url'] ?? null) }}"></a>
-                <p>Rating: {{ round($game->total_rating) }}/100</p>
-                <a href="/games/{{ $game->slug }}"><i class="fas fa-ellipsis-h"></i></a>
-            </div>
+            <x-game-card :game="$game"/>
             <hr>
         @endforeach
     </body>
