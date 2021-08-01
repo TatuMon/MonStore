f329585710dcce8877ca7c3d4c7dcd86b95915dd
TatuMon <ezurkanmn@gmail.com>
2021-08-01 03:16:47 -0300

    Newer games on query

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -8,7 +8,7 @@ use MarcReichel\IGDBLaravel\Models\Game;
 class GameController extends Controller
 {
     public function index(){
-        $newones = Game::whereHas('cover')->whereHas('websites')->whereHas('platforms')->with(['cover', 'websites', 'platforms'])->where('total_rating_count', '>=', 50)->orderByDesc('first_release_date')->get();
+        $newones = Game::whereHas('cover')->whereHas('websites')->whereHas('platforms')->with(['cover', 'websites', 'platforms'])->where('total_rating_count', '>=', 25)->orderByDesc('first_release_date')->get();
         
         return view('home', [
             'newones' => $newones
