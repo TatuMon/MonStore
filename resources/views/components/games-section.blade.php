0b95cdb70919594efa3e10fff20b9ec514ee24f8
TatuMon <ezurkanmn@gmail.com>
2021-07-31 22:41:03 -0300

    base carousel style

--- resources/views/components/games-section.blade.php
+++ resources/views/components/games-section.blade.php
@@ -1,23 +1,30 @@
 @props(['games'])
 
 <div class="section-wrapper">
-    @foreach ($games as $game)
-        <div id="game-{{ $loop->index }}" class="game-card">
-            <a href="/games/{{ $game->slug }}" style="inline-block">
-                <img src="{{ str_replace('t_thumb', 't_cover_big', $game->cover['url'] ?? null) }}">
-            </a>
-            <div class="info-wrapper">
-                <a class="game-name" href="/games/{{ $game->slug }}" style="inline-block">{{ $game->name }}</a>
-                <p class="game-rating">Rating: {{ round($game->total_rating) }}/100</p>
-                <p class="platforms">
-                    @foreach($game->platforms as $platform)
-                        {{ $platform['name'] }}
-                        @if(!$loop->last)
-                            | 
-                        @endif
-                    @endforeach
-                </p>
+    <h1 class="section-title">Novedades</h1>
+    <div class="carousel">
+        @foreach ($games as $game)
+            <div id="game-{{ $loop->index }}" class="game-card">
+                <a href="/games/{{ $game->slug }}" style="inline-block">
+                    <img src="{{ str_replace('t_thumb', 't_cover_big', $game->cover['url'] ?? null) }}">
+                </a>
+                <div class="info-wrapper">
+                    <a class="game-name" href="/games/{{ $game->slug }}" style="inline-block">{{ $game->name }}</a>
+                    <p class="game-rating">Rating: {{ round($game->total_rating) }}/100</p>
+                    <p class="platforms">
+                        @foreach($game->platforms as $platform)
+                            {{ $platform['name'] }}
+                            @if(!$loop->last)
+                                | 
+                            @endif
+                        @endforeach
+                    </p>
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
+    <div class="carousel-arrows-wrapper">
+        <div class="arrow-prev center-v"> &#10094; </div>
+        <div class="arrow-next center-v"> &#10095; </div>
+    </div>
 </div>
