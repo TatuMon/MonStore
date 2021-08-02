f9397173af485283567d47fc194d28edfa64a4f1
TatuMon <ezurkanmn@gmail.com>
2021-08-02 01:05:10 -0300

    Create and use layout component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>MonStore</title>
-
-        <script defer src="https://unpkg.com/alpinejs@3.0.1/dist/cdn.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-
-        <link href="css/app.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-    </head>
-    <body>
+<x-layout>
+        <x-slot name="title">MonStore</x-slot>
         <x-header />
         <div id="main">
             <x-games-section :games="$newones" :section="$section = ['index' => 0, 'name' => 'Most Recent']" id="novedades"/>
@@ -20,5 +7,4 @@
         </div>
 
         <script src="js/app.js"></script>
-    </body>
-</html>
+</x-layout>
