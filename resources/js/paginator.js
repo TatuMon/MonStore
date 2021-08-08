11278d834025c06efd7c926a9d4ba9ae60f62eb7
TatuMonNote <ezurkanmn@gmail.com>
2021-08-08 17:31:08 -0300

    Fixed route error

--- resources/js/paginator.js
+++ resources/js/paginator.js
@@ -1,6 +1,8 @@
+//Add new parameter to url
+//This allows to use paginator in both search and game route
+
 $('.pag-link').on('click', function(){
     var searchParams = new URLSearchParams(window.location.search);
     searchParams.set("page", $(this).text());
     window.location.search = searchParams.toString();
-    console.log('yikes')
 })
