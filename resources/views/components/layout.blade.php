bc8671a0a0211762afe3dc9b56f7c3ce035f9c2a
TatuMonNote <ezurkanmn@gmail.com>
2021-08-11 16:06:03 -0300

    Move navbar to a blade component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,25 +15,7 @@
     <body>
         <x-header />
         <div id="nav-container">
-            <nav>
-                <a href="/games">
-                    <i class="fas fa-gamepad"></i>
-                    Games
-                </a>
-                <a href="/collections">
-                    <i class="fas fa-layer-group"></i>
-                    Collections
-                </a>
-                <a href="/info">
-                    <i class="fas fa-info"></i>
-                    Info
-                </a>
-                <a href="https://github.com/TatuMon/MonStore.git" id="github" target="_blank">
-                    <i class="fab fa-github"></i>
-                    Repo
-                </a>
-            </nav>
-    
+            <x-navbar />
         </div>
         <div id="main">
             {{ $slot }}
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,18 @@
+<nav>
+    <a href="/games">
+        <i class="fas fa-gamepad"></i>
+        Games
+    </a>
+    <a href="/collections">
+        <i class="fas fa-layer-group"></i>
+        Collections
+    </a>
+    <a href="/info">
+        <i class="fas fa-info"></i>
+        Info
+    </a>
+    <a href="https://github.com/TatuMon/MonStore.git" id="github" target="_blank">
+        <i class="fab fa-github"></i>
+        Repo
+    </a>
+</nav>
